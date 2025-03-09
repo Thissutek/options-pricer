0df5863bc3015fcf58e452eb484cf542877237a1
Thissutek <jonathan_yau@outlook.com>
2025-03-08 23:44:05 -0500

    Added more layout to know where to put things, if main starts to get too crowded will start to split up the components

--- main.py
+++ main.py
@@ -6,11 +6,67 @@ app, rt = fast_app()
 
 @rt("/")
 def get():
-    inp = Label("Enter Current Stock Price:", Input(id="current-stock-price", name="current-stock-price", placeholder="Current Stock Price"))
+    desc = Container(
+        H1("Finance Made Easy"),
+        H4("How does Option Pricer Work"),
+        P("Loren ipsum"),
+        Ol(
+            Li("Enter Current Stock Price"),
+            Li("Enter Options Strike Price "),
+            Li("Enter the time of expiration"),
+            Li("Enter the Risk Free Interest Rate"),
+            Li("Enter the Volatility of the Stock")
+        )
+    )
+
+    inp = Container(
+            Grid(
+                Label(
+                    "Stock Price ($)",
+                    Input(id="current-stock-price", name="current-stock-price", placeholder="Current Stock Price")
+                ),
+                Label(
+                    "Option Price ($)",
+                    Input(id="option-strike-price", name="option-strike-price", placeholder="Option Strike Price")
+
+                ),
+                Label(
+                    "Time to expiration (years)",
+                    Input(id="time-expiration", name="time-expiration", placeholder="Time untill expiration"),
+
+                ),
+                Label(
+                    "Risk Free Interest Rate (%)",
+                    Input(id="risk-free-rate", name="risk-free-rate", placeholder="Risk Free Interest Rate"),
+
+                ),
+                Label(
+                    "Volatility (%)",
+                    Input(id="volatility-stock", name="volatility-stock", placeholder="Volatility of Stock")),
+
+                ),
+                Button("Click Me")
+            )
+    
+    map = Container(
+        H1("Heat Map")
+    )
+
+    options = Container(
+        H1("Options Price"),
+        Grid(
+            H3("Call Option Price"),
+            H3("Put Option Price")
+        )
+    )
+    
+
     return (Title("Options Pricer"), 
             Div(
-                H1("Finance Made Easy"),
-                inp
+                desc,
+                inp,
+                map,
+                options
             ))
 
 
